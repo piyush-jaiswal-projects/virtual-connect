921b83f846bff219f9146c8d84dc35d9d92fbeb6
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-11-20 13:26:58 +0530

    add formik lib

--- client/src/components/signin/form.tsx
+++ client/src/components/signin/form.tsx
@@ -3,35 +3,21 @@ import OtpForm from "./otp-form";
 import $ from "jquery";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { Formik } from "formik";
+
+type FormValues = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 export default function SigninForm() {
   const [passType, setPassType] = React.useState("password");
-  const [form, setForm] = React.useState({
-    name: "",
-    email: "",
-    password: "",
-  });
 
-  const [status, setStatus] = React.useState({
-    isLoading: false,
-    msg: "",
-    isError: false,
-  });
-
-  const submitDetails = async () => {
-    if (form.email.length === 0 || form.password.length === 0) {
-      setStatus({
-        ...status,
-        isError: true,
-        msg: "Please enter email and password",
-      });
-      return;
-    }
-
+  const submitDetails = async (values: FormValues) => {
     toast.loading("Signing up ...", { toastId: "loading-signin-form" });
-
     axios
-      .post(`http://localhost:5001/api/user/signin`, form)
+      .post(`http://localhost:5001/api/user/signin`, values)
       .then((response) => {
         const { message, process, success } = response.data;
 
@@ -44,7 +30,9 @@ export default function SigninForm() {
             $("#otp-form").removeClass("hidden");
             return;
           }
-          toast.info("Redirecting to dashboard ...", { toastId: "info-dashboard" });
+          toast.info("Redirecting to dashboard ...", {
+            toastId: "info-dashboard",
+          });
           setTimeout(() => window.location.replace("/dashboard"), 5000);
         }
       })
@@ -56,79 +44,130 @@ export default function SigninForm() {
 
   return (
     <>
-      <OtpForm email={form.email} />
-      <div
-        id="signin-form"
-        className="p-4 w-[95vw] lg:w-[30vw] bg-white mx-auto relative lg:top-[50px] lg:translate-y-[-150px] border rounded-lg"
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        validate={(values) => {
+          var errors: FormValues = {
+            name: "",
+            email: "",
+            password: "",
+          };
+          if (!values.email) {
+            // validate email
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+
+          if (!values.password) {
+            // validate password
+            errors.password = "Required";
+          }
+
+          if (!values.name) {
+            // validate name
+            errors.name = "Required";
+          }
+
+          return errors;
+        }}
+
+        onSubmit={async (values) => {
+          console.log("clicked");
+          
+          try {
+            submitDetails(values);
+          } catch (error: any) {
+            toast.error(error.message);
+          }
+        }}
       >
-        <h1 className="text-[1.8rem]">Signin</h1>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit
+        }) => (
+          <>
+            <OtpForm email={values.email} />
+            <div
+              id="signin-form"
+              className="p-4 w-[95vw] lg:w-[30vw] bg-white mx-auto relative lg:top-[50px] lg:translate-y-[-150px] border rounded-lg"
+            >
+              <h1 className="text-[1.8rem]">Signin</h1>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="w-[100%] border outline-none rounded-lg h-[40px] p-2 mt-4"
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {<small>{errors.name && touched.name && errors.name}</small>}
 
-        <p className={status.isError ? "text-[red]" : "text-[green]"}>
-          {status.msg}
-        </p>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="w-[100%] border outline-none rounded-lg h-[40px] p-2 mt-4"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {<small>{errors.email && touched.email && errors.email}</small>}
 
-        <input
-          type="text"
-          placeholder="Name"
-          className="w-[100%] border outline-none rounded-lg h-[40px] p-2 my-2"
-          value={form.name}
-          onChange={(e) => {
-            setForm({
-              ...form,
-              name: e.target.value,
-            });
-          }}
-        />
+                <input
+                  type={passType}
+                  name="password"
+                  placeholder="Password"
+                  className="w-[100%] border outline-none rounded-lg h-[40px] p-2 mt-4"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {
+                  <small>
+                    {errors.password && touched.password && errors.password}
+                  </small>
+                }
 
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-[100%] border outline-none rounded-lg h-[40px] p-2 my-2"
-          value={form.email}
-          onChange={(e) => {
-            setForm({
-              ...form,
-              email: e.target.value,
-            });
-          }}
-        />
+                <br />
 
-        <input
-          type={passType}
-          placeholder="Password"
-          className="w-[100%] border outline-none rounded-lg h-[40px] p-2 my-2"
-          value={form.password}
-          onChange={(e) => {
-            setForm({
-              ...form,
-              password: e.target.value,
-            });
-          }}
-        />
+                <div className="mt-4 mb-2">
+                  <input
+                    id="show-pass"
+                    type="Checkbox"
+                    placeholder="Show Password"
+                    onClick={() => {
+                      if ($("#show-pass").is(":checked")) {
+                        setPassType("text");
+                      } else {
+                        setPassType("password");
+                      }
+                    }}
+                  />
+                  <label className="text-gray-500"> Show password</label>
+                </div>
 
-        <input
-          id="show-pass"
-          type="Checkbox"
-          placeholder="Show Password"
-          onClick={() => {
-            if ($("#show-pass").is(":checked")) {
-              setPassType("text");
-            } else {
-              setPassType("password");
-            }
-          }}
-        />
-        <label className="text-gray-500"> Show password</label>
-
-        <br />
-
-        <button
-          onClick={submitDetails}
-          className="bg-[black] border hover:bg-[#ffffff] hover:text-black my-2 h-[50px] w-[100%] rounded-lg p-2 text-white"
-        >
-          Submit
-        </button>
-      </div>
+                <button
+                  type="submit"
+                  onClick={() => submitDetails(values)
+                  }
+                  className="bg-[black] border hover:bg-[#ffffff] hover:text-black my-2 h-[50px] w-[100%] rounded-lg p-2 text-white"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </Formik>
     </>
   );
 }
